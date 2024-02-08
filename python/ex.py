9daebf7e4b2b1a8d6cc7fa896c75cfc55f40561a
Seou0912 <myuni0912@gmail.com>
2024-02-08 19:25:55 +0900

    0208

---
+++ python/ex.py
@@ -0,0 +1,38 @@
+print('Makit "code" \lab')
+print("she's gone")
+
+a = 10
+b = 20
+print("a 의 값은 ", a)
+print("b 의 값은 ", b)
+print("a와 b의 합은 ", a + b)
+
+a = 30
+b = 8
+
+print(a, "을", b, "로 나눈 몫은", a // b)
+print(a, "을", b, "로 나눈 나머지는", a % b)
+print(a, "을", b, "로 나눈 결과는 ", a / b)
+
+a = 30 // 8
+b = 30 % 8
+
+print("30을 8로 나눈 몫은", a)
+print("30을 8로 나눈 나머지는", b)
+print("30을 8로 나눈 결과는 ", 30 / 8)
+
+# name = input("이름이 무엇인가요?")
+# age = int(input("몇 살인가요?"))
+# age += 1
+
+# print(name, "님은 내년에는", age, "살이 됩니다.")
+
+
+a = int(input())
+b = float(input())
+
+c = a + b
+d = c / 2
+
+print("a와 b의 합은 ", c)
+print("a와 b의 평균값은 ", d)
